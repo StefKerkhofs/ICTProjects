6ae19fc8cfc93835ea72a9980a7fe8f3761367c9
Stef Kerkhofs <kerkhofsstef@hotmail.com>
2018-05-08 14:16:52 +0200

    Added AdminStudyController

---
+++ app/Http/Controllers/AdminStudyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Page;
+use Illuminate\Http\Request;
+
+/**
+ * Class AdminStudyController
+ * @package App\Http\Controllers
+ * @author Stef Kerkhofs
+ */
+
+
+
+class AdminStudyController extends Controller
+{
+    /**
+     *getList()
+     * Retrieves the list containing all the studies and passes it to the view
+     *
+     * @author Stef Kerkhofs
+     * @since 2018-05-08
+     */
+}
