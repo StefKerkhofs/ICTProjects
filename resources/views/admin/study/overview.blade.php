72f966eb3f68d83b0bf6915c6316c35bba523bea
Stef Kerkhofs <kerkhofsstef@hotmail.com>
2018-05-10 17:38:37 +0200

    changed css study

--- resources/views/admin/study/overview.blade.php
+++ resources/views/admin/study/overview.blade.php
@@ -1,39 +1,56 @@
 @extends('admin.templates.templateBackend')
 @section('content')
+<style>
+    #content-left
+    {width: 40%;
+        display:flex;
 
+        justify-content: flex-start;
+    }
+    #content-right
+    {width: 40%;
+        display:flex;
+
+        justify-content: flex-end;
+    }
+
+
+</style>
+<div id="content-left">
     <table id="studyTable">
         <thead>
         <tr>
-            <th> Study ID</th>
-            <th> Study Name</th>
+            <th>Richtingen</th>
         </tr>
         </thead>
         <tbody>
         @foreach($aStudyData = DB::table('studies')->get() as $oStudy)
             <tr>
-                <td> {{$oStudy->study_id}} </td>
                 <td> {{$oStudy->study_name}} </td>
             </tr>
         @endforeach
         </tbody>
     </table>
-
+</div>
+<div id="content-right">
     <table id="majorTable">
         <thead>
         <tr>
-            <th> Major ID</th>
-            <th> Major Name</th>
+            <th>Afstudeerrichtingen</th>
+
         </tr>
         </thead>
         <tbody>
         @foreach($aMajorData = DB::table('majors')->get() as $oMajor)
             <tr>
-                <td> {{$oMajor->major_id}} </td>
                 <td> {{$oMajor->major_name}} </td>
             </tr>
         @endforeach
         </tbody>
     </table>
+</div>
+
+
 
 
 
