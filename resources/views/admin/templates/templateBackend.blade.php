e4f419c1cca9a45698a3a75ebd12c625a402b76f
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-05-10 17:43:07 +0200

    links

--- resources/views/admin/templates/templateBackend.blade.php
+++ resources/views/admin/templates/templateBackend.blade.php
@@ -62,7 +62,7 @@
         }
         section#content{
             background-color: white;
-            margin: 50px 50px 50px 250px; 
+            margin: 50px 50px 50px 250px;
         }
     </style>
 </head>
@@ -80,11 +80,10 @@
 </header>
 <nav id="side-bar">
     <ul>
-        <a href=""><li>Dashboard</li></a>
-        <a href=""><li>Info</li></a>
-        <a href=""><li>Reizen</li></a>
-        <a href=""><li>Postcodes</li></a>
-        <a href=""><li>Richtingen</li></a>
+        <a href="{{ url('admin/info') }}"><li>Info</li></a>
+        <a href="{{ url('admin/trip') }}"><li>Reizen</li></a>
+        <a href="{{ url('admin/zip') }}"><li>Postcodes</li></a>
+        <a href="{{ url('admin/study') }}"><li>Richtingen</li></a>
         <a href=""><li>PDF</li></a>
         <a href=""><li>Contact</li></a>
     </ul>
