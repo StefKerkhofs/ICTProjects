6c86b76be47400b64716984e978336b036a99300
Joren Meynen <joren.meynen@telenet.be>
2018-05-14 12:59:11 +0200

    main

---
+++ resources/views/user/inc/nav.blade.php
@@ -0,0 +1,25 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light headbar">
+    <img src="{{asset("/image/ucll_3493.png")}}" alt="ucll logo" height="55" width="120"/>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarS" aria-controls="navbarS" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarS">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Info <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Reis 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"  href="#">Reis 2</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
