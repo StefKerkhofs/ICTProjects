dd2f3950cef5ec937b6b9c83cfbd776b02ad63d1
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-05-14 15:54:26 +0200

    Register progress

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,13 +6,34 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    function returnv($iViewID){
-        switch ($iViewID){
+    function returnv(){
+
+        if(!isset($_COOKIE['register'])) {
+            $this->CreateCookie();
+        }
+        else{
+            $this->ShowView();
+        }
+    }
+    function ShowView(){
+        $aData = unserialize($_COOKIE['register']);
+        switch ($aData['PageID']){
             case 1:
                 return view('user.register.form1');
                 break;
             case 2:
+                return view('user.register.form2');
                 break;
         }
+    }
+
+    function CreateCookie(){
+        $cookie_name = "register";
+        $iRegisterCount = 1;
+        $aUserData = null;
+        $aUserData['PageID'] = 1;
+        $cookie_value = $aUserData;
+        setcookie($cookie_name, serialize($cookie_value), time() + (86400 * 30), "/");
+        $this->ShowView();
     }
 }
