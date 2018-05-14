e8f4dbf55b06030cd5c40c32a665c375d3760e17
Joren Meynen <joren.meynen@telenet.be>
2018-05-15 01:50:19 +0200

    searchStudent.blade.php opmaak

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile', 'ProfileController@profile');
-Route::get('/editprofile', 'ProfileController@editprofile');
-Route::get('/profileEdit', 'ProfileController@profileEdit');
 Route::get('/searchStudentEdit', 'ProfileController@searchStudentEdit');
 Route::get('/{page}', 'GuestPagesController@showPage');
 Route::get('/profileEdit', 'ProfileController@profileEdit');
-Route::get('/searchStudentEdit', 'ProfileController@searchStudentEdit');
