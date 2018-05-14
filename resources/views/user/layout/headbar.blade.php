22b2a9e1c419153350b43e76ff4f4f6f4d5b2ffc
Nick Castermans <nickcastermans@MacBook-Pro-van-Nick.local>
2018-05-14 15:28:31 +0200

    template aanpassingen

--- resources/views/user/layout/headbar.blade.php
+++ resources/views/user/layout/headbar.blade.php
@@ -1,6 +1,6 @@
 
 
-<nav class="navbar navbar-expand-lg navbar-light headbar">
+<nav class=" navbar navbar-expand-lg navbar-dark headbar">
     <img src="{{asset("/image/ucll_3493.png")}}" alt="ucll logo" height="55" width="120"/>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarS" aria-controls="navbarS" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
