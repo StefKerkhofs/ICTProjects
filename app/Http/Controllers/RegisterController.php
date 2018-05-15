7c1252069e696e8081adff13677e1a425796d140
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-05-15 13:49:33 +0200

    Register progress

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,51 +6,52 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    function returnv(){
-        if(!isset($_COOKIE['register'])) {
-            $aData['PageID'] = 1;
-            $aData['PageTest'] = 'Test';
-            setcookie("register", serialize($aData), time() + (86400 * 30), "/");
-            $_COOKIE['register'] = serialize($aData);
-        }
-        return $this->ShowView();
-    }
-    function next(){
-        $aData = unserialize($_COOKIE['register']);
-        $aData['PageID']++;
-        $aData['PageTest'] = 'Next';
-        setcookie("register", serialize($aData), time() + (86400 * 30), "/");
-        $_COOKIE['register'] = serialize($aData);
-        return $this->ShowView();
-    }
-    function previous(){
-        $aData = unserialize($_COOKIE['register']);
-        $aData['PageID']--;
-        $aData['PageTest'] = 'Previous';
-        setcookie("register", serialize($aData), time() + (86400 * 30), "/");
-        $_COOKIE['register'] = serialize($aData);
-        return $this->ShowView();
-    }
-
+    private $aData = null;
     public function form1(){
+        setcookie("register", serialize($this->aData), time() + (86400 * 30), "/");
         return view('user.register.form1');
     }
+
     public function form2(Request $aRequest){
-        $aDate['nummer'] = $aRequest->post('txtNummer');
-        $aDate['wachtwoord'] = $aRequest->post('txtWachtwoord');
-        echo $aDate['nummer'];
+        if (!isset($_COOKIE['register'])){
+            return $this->form1();
+        }
+        $this->aDate['nummer'] = $aRequest->post('txtNummer');
+        $this->aDate['wachtwoord'] = $aRequest->post('txtWachtwoord');
         return view('user.register.form2');
     }
-     public function form3(){
-        return view('user.register.form3');
+
+     public function form3(Request $aRequest){
+         if (!isset($_COOKIE['register'])){
+             return $this->form1();
+         }
+        $this->aData['reis'] = $aRequest->post('ReisKiezen');
+        $this->aData['afstudeerRichting'] = $aRequest->post('AfstudeerrichtingKiezen');
+         echo $this->aData['nummer'];
+         echo $this->aData['wachtwoord'];
+         echo $this->aData['reis'];
+         echo $this->aData['afstudeerRichting'];
+         return view('user.register.form3');
     }
-    public function form4(){
+
+    public function form4(Request $aRequest){
+        if (!isset($_COOKIE['register'])){
+            return $this->form1();
+        }
         return view('user.register.form4');
     }
+
     public function form5(){
+        if (!isset($_COOKIE['register'])){
+            return $this->form1();
+        }
         return view('user.register.form5');
     }
+
     public function form6(){
+        if (!isset($_COOKIE['register'])){
+            return $this->form1();
+        }
         return view('user.register.form6');
     }
 }
