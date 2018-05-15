da4d83a7e177c85e00a411625ab7bb4754087989
Robin <robin.machiels@gmail.com>
2018-05-15 13:49:40 +0200

    ProfileController met database

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,29 @@ class ProfileController extends Controller
 {
     public function profile()
     {
+        if(Auth::check()) {
+            $id = Auth::id();
+
+            $aUser = DB::select('select * from travellers where user_id = :id', ['id' => $id]);
+            $oTraveller = $aUser[0];
+            $aTravellerData = array();
+            $aTravellerData['firstname'] = $oTraveller->firstname;
+            $aTravellerData['lastname'] = $oTraveller->lastname;
+            $aTravellerData['city'] = $oTraveller->city;
+            $aTravellerData['country'] = $oTraveller->country;
+            $aTravellerData['address'] = $oTraveller->address;
+            $aTravellerData['sex'] = $oTraveller->sex;
+            $aTravellerData['email'] = $oTraveller->email;
+            $aTravellerData['phone'] = $oTraveller->phone;
+            $aTravellerData['emergencyPhone1'] = $oTraveller->emergency_phone_1;
+            $aTravellerData['emergencyPhone2'] = $oTraveller->emergency_phone_2;
+            $aTravellerData['nationality'] = $oTraveller->nationality;
+            $aTravellerData['birthdate'] = $oTraveller->birthdate;
+            $aTravellerData['birthplace'] = $oTraveller->birthplace;
+            $aTravellerData['medicalInfo'] = $oTraveller->medical_info;
+
+            return view('user.profile.profile', ['traveller' => $aTravellerData]);
+        }
         return view('user.profile.profile');
     }
     public function profileEdit()
