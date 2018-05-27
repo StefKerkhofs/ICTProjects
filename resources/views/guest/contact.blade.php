8f82aaa9c67b6bcde7a48606f5693fa76508eb6d
Kaan Akpinar <kaan.akpinar@hotmail.com>
2018-05-27 19:29:51 +0200

    contact pagina styling

--- resources/views/guest/contact.blade.php
+++ resources/views/guest/contact.blade.php
@@ -1,9 +1,36 @@
 @extends('user.templates.templateFrontEnd')
-
+@section('style')
+    <style>
+        table, th, td {
+            border: 1px solid darkgray;
+            border-collapse: collapse;
+        }
+        th{
+            text-align: center;
+            background-color: #e00049;
+            color: #E9F3F8;
+        }
+        th:not(:last-child){
+            height: 50px;
+            width: 250px;
+        }
+        td{
+            padding: 15px 30px;
+            text-align: right;
+        }
+    </style>
+@endsection
 @section('content')
+
     <div class="container">
         <H1>Contactpagina</H1>
         <table>
+            <tr>
+                <th>Reis</th>
+
+                <th>Mail contactpersoon</th>
+
+            </tr>
             @foreach($aActiveTrips as $aActiveTrip)
                 <tr>
                     <td>{{ $aActiveTrip->trip_name }}</td>
