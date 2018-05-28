dee888055470acf6fc2f86463ae53c2b16c2d669
Joren Meynen <joren.meynen@telenet.be>
2018-05-28 16:33:36 +0200

    relation menu - page

--- app/Menu.php
+++ app/Menu.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    protected $fillable = [
+        'menu_id',
+        'page_id',
+        'menu_name'
+    ];
+    public function page()
+    {
+        return $this->belongsTo('App\Page');
+    }
 }
