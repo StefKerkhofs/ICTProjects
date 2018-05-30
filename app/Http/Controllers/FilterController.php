3bcf9ea784726138c70b3494ed089cbefc817c50
Michiel <Michiel1998@gmail.com>
2018-05-31 00:34:09 +0200

    added auth::check

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -18,27 +18,29 @@ class FilterController extends Controller
 {
     public function getFilteredTraveller(Request $request)
     {
-        $id = Auth::id();
-        $afilters=$request->all();
-        array_shift($afilters);
+        if(Auth::check()) {
+            $id = Auth::id();
+            $afilters = $request->all();
+            array_shift($afilters);
 
-        $tripid=DB::table('travellers')
-            ->where('user_id',$id)
-            ->select('trip_id')
-            ->get();
-        $tripid = json_decode(json_encode($tripid),true);
+            $tripid = DB::table('travellers')
+                ->where('user_id', $id)
+                ->select('trip_id')
+                ->get();
+            $tripid = json_decode(json_encode($tripid), true);
 
-        $afilteredUserList=DB::table('travellers')
-            ->join('studies', 'travellers.study_id', '=', 'studies.study_id')
-            ->join('trips', 'travellers.trip_id', '=', 'trips.trip_id')
-            ->where('travellers.trip_id',$tripid[0])
-            ->get();
-        $afilteredUserList = json_decode(json_encode($afilteredUserList),true);
-        return view('user.filter.filter', [
-            'afilteredUserList' => $afilteredUserList,
-            'afilters'=>$afilters,
-            'tripid'=>$tripid
-        ]);
+            $afilteredUserList = DB::table('travellers')
+                ->join('studies', 'travellers.study_id', '=', 'studies.study_id')
+                ->join('trips', 'travellers.trip_id', '=', 'trips.trip_id')
+                ->where('travellers.trip_id', $tripid[0])
+                ->get();
+            $afilteredUserList = json_decode(json_encode($afilteredUserList), true);
+            return view('user.filter.filter', [
+                'afilteredUserList' => $afilteredUserList,
+                'afilters' => $afilters,
+                'tripid' => $tripid
+            ]);
+        }
     }
 
 
