b43f9ec348c772108322ac044eea1ad4cecfcf00
Nick Castermans <nickcastermans@MacBook-Pro-van-Nick.local>
2018-05-31 14:10:51 +0200

    clean

--- resources/views/user/layout/userbar.blade.php
+++ resources/views/user/layout/userbar.blade.php
@@ -5,29 +5,21 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mr-3">
-            <?php
-            if (\Illuminate\Support\Facades\Auth::user())
-            {
-                $sUserFirstname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('firstname');
-                $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
-                ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">Welcome, <?php echo $sUserFirstname , " " , $sUserLastname ?></a>
-                </li>
-                <?php
-            }
-            ?>
-            <li class="nav-item ">
-                <a class="nav-link" href="/profile">Profile<span class="sr-only">(current)</span></a>
-            </li>
             @guest
             <li class="nav-item">
                 <a class="nav-link" href="/logInUser">Log in</a>
             </li>
             @else
-                <li class="nav-item">
-                    <a class="nav-link" href="/logoutUser">Log out</a>
-                </li>
+                    <?php
+                        $sUserFirstname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('firstname');
+                        $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/profile">Welcome, <?php echo $sUserFirstname , " " , $sUserLastname ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logoutUser">Log out</a>
+                    </li>
             @endguest
         </ul>
     </div>
