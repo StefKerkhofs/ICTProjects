c8186b5ea6ff456c24b5dea90d5b700fe5364aa6
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-06-08 16:42:09 +0200

    Revert "Merge branch 'back-end'"
    This reverts commit 16a90e77f831c4886eed1b79c1350628bb027d05, reversing
    changes made to 2072ea976a1b1408ab12b6664c6c45dea5e74ff4.

--- resources/views/layouts/app.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-
-@extends('user.templates.templateFrontEnd')
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</html>
