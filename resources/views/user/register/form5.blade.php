2072ea976a1b1408ab12b6664c6c45dea5e74ff4
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-06-08 16:39:36 +0200

    Master Clean

--- resources/views/user/register/form5.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('user.templates.templateFrontEnd')
-@section('style')
-
-@endsection
-@section('content')
-    <h1>Page5</h1>
-@endsection
--- storage/framework/views/896a0591961e0eb723a2b8566374fc937b28108d.php
+++
@@ -1,1 +0,0 @@
-<footer id="footer"><?php echo e(date('Y')); ?> - <?php echo e(config('app.name')); ?></footer>
