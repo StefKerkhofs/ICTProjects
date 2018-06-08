0db811285a8b27c7939e45c012d61ab788c0ae17
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-06-08 15:53:44 +0200

    Front-end Controllers

---
+++ app/Http/Controllers/FilterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traveller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class FilterController
+ * @package App\Http\Controllers
+ * @author Michiel Guilliams
+ */
+
+class FilterController extends Controller
+{
+    public function getFilteredTraveller(Request $request)
+    {
+        if (\App\User::where('id',\Illuminate\Support\Facades\Auth::id())->value('function') !== 'Begeleider')
+        {
+            return redirect('/info');
+        }
+        //check if user is logged in
+        if(Auth::check()) {
+            //find the mentor's user_id
+            $id = Auth::id();
+
+            //find the checked filters
+            $afilters = $request->all();
+            array_shift($afilters);
+
+            //find the mentor's trip_id
+            $tripid = DB::table('travellers')
+                ->where('user_id', $id)
+                ->select('trip_id')
+                ->first();
+            $tripid = json_decode(json_encode($tripid), true);
+
+            //find all travellers with the same trip_id
+            $afilteredUserList = DB::table('travellers')
+                ->join('studies', 'travellers.study_id', '=', 'studies.study_id')
+                ->join('trips', 'travellers.trip_id', '=', 'trips.trip_id')
+                ->join('users','travellers.user_id','=','users.id')
+                ->where('travellers.trip_id', $tripid)
+                ->get();
+            $afilteredUserList = json_decode(json_encode($afilteredUserList), true);
+
+            //return travellers and selected filters to list
+            return view('user.filter.filter', [
+                'afilteredUserList' => $afilteredUserList,
+                'afilters' => $afilters,
+                'tripid' => $tripid
+            ]);
+        }
+        //if user is not logged in
+        return redirect('/');
+    }
+}
---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Traveller;
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function profile()
+    {
+
+        $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
+        if ($sUserLastname == null){
+            return redirect('/');
+        }
+
+        //check if user is logged in
+        if(Auth::check()) {
+            //find the user's user_id
+            $id = Auth::id();
+
+            //find the user's traveller_id
+            $oTraveller = DB::table('travellers')
+                ->join('users', 'travellers.user_id', '=', 'users.id')
+                ->where('travellers.user_id', '=', $id)
+                ->first();
+            $oTraveller = json_decode(json_encode($oTraveller),true);
+
+            return view('user.profile.profile', ['traveller' => $oTraveller]);
+        }
+
+        //if user is not logged in
+        return redirect('/');
+    }
+    public function profileEdit()
+    {
+        $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
+        if ($sUserLastname == null){
+            return redirect('/');
+        }
+
+        //check if user is logged in
+        if(Auth::check()) {
+            //find the user's user_id
+            $id = Auth::id();
+
+            //find the user's traveller_id
+            $aTravellers = DB::table('travellers')
+                ->join('users', 'travellers.user_id', '=', 'users.id')
+                ->where('travellers.user_id', '=', $id)
+                ->get();
+            //echo $aRequest;
+            return view('user.profile.profileEdit', ['aTravellers' => $aTravellers]);
+        }
+        //if user is not logged in
+        return redirect('/');
+    }
+
+    public function profileUpdate(Request $aRequest)
+    {
+        $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
+        if ($sUserLastname == null){
+            return redirect('/');
+        }
+
+        //check if user is logged in
+        if(Auth::check()) {
+
+            $aRequest->validate([
+                'txtLastName' => 'required',
+                'txtFirstName' => 'required',
+                'txtSex' => 'required',
+                'txtBirthdate' => 'required|date_format:d/m/Y',
+                'txtBirthplace' => 'required',
+                'txtNationality' => 'required',
+                'txtAddress' => 'required',
+                'txtCity' => 'required',
+                'txtCountry' => 'required',
+
+                'txtEmail' => [ 'required', 'string', 'email', 'max:255' /*, 'unique:users,email' */],
+                'txtPhone' => 'required',
+                'txtEmergencyPhone1' => 'required',
+                'txtMedicalIssue' => 'required',
+            ],$this->messages());
+
+            //find the user's user_id
+            $id = Auth::id();
+
+            //update the user's profile
+            DB::table('travellers')
+                ->join('users', 'travellers.user_id', '=', 'users.id')
+                ->where('travellers.user_id', '=', $id)
+                ->update(
+                    [
+                        'lastname'          => $aRequest->post('txtLastName'),
+                        'firstname'         => $aRequest->post('txtFirstName'),
+                        'sex'               => $aRequest->post('txtSex'),
+                        'birthdate'         => $aRequest->post('txtBirthdate'),
+                        'birthplace'        => $aRequest->post('txtBirthplace'),
+                        'nationality'       => $aRequest->post('txtNationality'),
+                        'address'           => $aRequest->post('txtAddress'),
+                        'city'              => $aRequest->post('txtCity'),
+                        'country'           => $aRequest->post('txtCountry'),
+                        'email'             => $aRequest->post('txtEmail'),
+                        'phone'             => $aRequest->post('txtPhone'),
+                        'emergency_phone_1' => $aRequest->post('txtEmergencyPhone1'),
+                        'emergency_phone_2' => $aRequest->post('txtEmergencyPhone2'),
+                        'MedicalIssue'      => $aRequest->post('txtMedicalIssue'),
+                        'medical_info'      => $aRequest->post('txtMedicalInfo'),
+                    ]
+                );
+
+            return redirect('/profile');
+        }
+        //if user is not logged in
+        return redirect('/');
+    }
+
+    public function messages()
+    {
+        return [
+            'txtLastName.required' => 'Je moet je achternaam invullen.',
+            'txtFirstName.required' => 'Je moet je voornaam invullen.',
+            'txtSex.required' => 'Je moet een geslacht kiezen.',
+            'txtBirthdate.required' => 'Je moet je geboorte datum ingeven.',
+            'txtBirthdate.date_format' => 'De waarde die je hebt ingevuld hebt bij geboorte datum in ongeldig',
+            'txtBirthplace.required' => 'Je moet je geboorte plaats ingeven.',
+            'txtNationality.required' => 'je moet je nationaliteit opgeven.',
+            'txtAddress.required' => 'Je moet je adres ingeven.',
+            'txtCity.required' => 'Je moet je postcode ingeven.',
+            'txtCountry.required' => 'Je moet je land ingeven',
+
+            'txtEmail.required' => 'Vul je email adres in.',
+            'txtEmail.email' => 'Het ingevulde email adres is niet geldig.',
+            'txtEmail.unique' => 'Het ingevulde email adres is al in gebruik.',
+            'txtPhone.required' => 'Je moet je GSM nummer invullen.',
+            'txtEmergencyPhone1.required' => 'Je moet minstens 1 noodnummer invullen.',
+            'txtMedicalIssue.required' => 'Je moet aanduiden indien je een medische behandeling volgt of niet.',
+        ];
+    }
+}
