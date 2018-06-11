93ef26be02ed6eab12a028a2115ec80516f072c2
Nick Castermans <nickcastermans@MacBook-Pro-van-Nick.local>
2018-06-11 09:28:09 +0200

    title + clean

--- resources/views/user/layout/userbar.blade.php
+++ resources/views/user/layout/userbar.blade.php
@@ -2,7 +2,6 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mr-3">
             @guest
@@ -14,7 +13,6 @@
                         $sUserFirstname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('firstname');
                         $sUserLastname = \App\Traveller::where('user_id',\Illuminate\Support\Facades\Auth::id())->value('lastname');
                         if ($sUserLastname !== null){
-
                     ?>
                     <li class="nav-item">
                         <a class="nav-link" href="/profile">Welcome <?php echo $sUserFirstname,  " " , $sUserLastname ?></a>
